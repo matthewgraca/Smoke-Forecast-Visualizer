699ddaf30c3a7f13879cc284176fe733cf8c6bea
Matthew Graca <matthew.graca@gmail.com>
2025-05-07 15:10:21 -0700

    included basic functionality to plot a 200x200m green, translucent square in LA

--- app/src/main/java/com/example/smokeforecastvisualizer/MapsActivity.kt
+++ app/src/main/java/com/example/smokeforecastvisualizer/MapsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.smokeforecastvisualizer
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,39 +12,37 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.smokeforecastvisualizer.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.GroundOverlayOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Center the map on Los Angeles
+        val losAngeles = LatLng(34.0522, -118.2437)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(losAngeles, 14f))
+
+        // Create a 1x1 pixel bitmap with translucent green
+        val bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+        bitmap.setPixel(0, 0, Color.argb(128, 0, 255, 0)) // 128 = 50% transparency
+
+        // Add it to the map using a GroundOverlay
+        val overlay = GroundOverlayOptions()
+            .image(BitmapDescriptorFactory.fromBitmap(bitmap))
+            .position(losAngeles, 200f, 200f) // 20m x 20m area
+        map.addGroundOverlay(overlay)
     }
 }
